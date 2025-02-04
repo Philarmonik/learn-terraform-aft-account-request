832a9334904599d0b5fd5aea26f6ed68083660fb
Philippe Chastagnol <philippe.chastagnol.external@airbus.com>
2025-02-04 04:50:56 +0100

    add new module sandbox2 for sandbox account creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,68 @@
+# module "sandbox" {
+#   source = "./modules/aft-account-request"
+
+#   control_tower_parameters = {
+#     AccountEmail              = "aws.lz.phil+thesandbox@gmail.com"
+#     AccountName               = "sandbox-aft"
+#     ManagedOrganizationalUnit = "Sandbox"
+#     SSOUserEmail              = "aws.lz.phil+thesandbox@gmail.com"
+#     SSOUserFirstName          = "sandbox"
+#     SSOUserLastName           = "AFT"
+#   }
+
+#   account_tags = {
+#     "Learn Tutorial" = "AFT"
+#   }
+
+#   change_management_parameters = {
+#     change_requested_by = "HashiCorp Learn"
+#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+#   }
+
+#   custom_fields = {
+#     group = "non-prod"
+#   }
+
+#   account_customizations_name = "sandbox"
+# }
+
+
 module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "aws.lz.phil+thesandbox@gmail.com"
+    AccountEmail              = "aws.lz.phil+account1@gmail.com"
+    AccountName               = "account1-aft"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "aws.lz.phil+account1@gmail.com"
+    SSOUserFirstName          = "account1"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
+module "sandbox2" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws.lz.phil+sandbox@gmail.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "aws.lz.phil+thesandbox@gmail.com"
+    SSOUserEmail              = "aws.lz.phil+sandbox@gmail.com"
     SSOUserFirstName          = "sandbox"
     SSOUserLastName           = "AFT"
   }
